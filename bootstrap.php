95293bfc52076c8ecf5d9e5b65de42b0b37079b5
wesley.sartori <wesley.sartori@ixcsoft.com.br>
2024-07-10 00:00:39 -0300

    feat(reservas): Reservas
    Configurações iniciais do docker

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
 require 'vendor/autoload.php';
+
+require_once 'public/index.php';
--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 use App\Core\Routes;
 use App\Core\View;
 
-require '../bootstrap.php';
-
 try {
     Routes::load();
 } catch (Exception $exception) {
